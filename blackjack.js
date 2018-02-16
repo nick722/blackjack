972a353291becc99df8e52c4d9ceac24392b885f
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-02-16 17:58:19 +0300

    commit 180216 0558

--- blackjack.js
+++ blackjack.js
@@ -1,23 +1,41 @@
-//https://www.codecademy.com/courses/blackjack-part-1/1/3
+/*
+https://www.codecademy.com/courses/blackjack-part-1/1/6
+*/
 
 // Our deal function will return a random card
-function deal() {
+var deal = function() {
     card = Math.floor(Math.random()*52+1);
     return card;
-}
+};
 
 // Deal out our first hand
 var card1 = deal();
 var card2 = deal();
 
-// Make a getValue function here, which should convert a card to
-// the value that card is worth
-function getValue
+// This function takes a card as a parameter and returns the value
+// of that card
+var getValue = function(card) {
+    // if its a face card, number should be set to 10
+    if( card % 13 === 0 ||
+        card % 13 === 11 ||
+        card % 13 === 12) {
+        return 10;
+    }
+    // What if it's an ace?
+    else if (card % 13 === 1) {
+        return 11;
+    }
 
-// Our score function converts our cards to a score
-var score = function () {
+    // Otherwise, we just want its number value
+    else {
+        return card % 13;
+    }
+}
+
+// Score the hand
+function score() {
     return getValue(card1) + getValue(card2);
-};
+}
 
 console.log("You have cards " + card1 + " and " + card2 +
-    " for a score of " + score());
+    " for a score of " + score(card1, card2));
